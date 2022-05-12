2ffc9df24caea82e90ef5ee129433255fb0d27c2
Guilherme Garcia <garciaguig@gmail.com>
2022-05-12 16:25:36 -0300

    Atividade 2: Conclusão + Alterações no README

--- modulo-01-fundamentos/bloco-04_introducao-javascript-logica-programacao/dia-01_javascript-primeiros-passos/ativ-02.js
+++ modulo-01-fundamentos/bloco-04_introducao-javascript-logica-programacao/dia-01_javascript-primeiros-passos/ativ-02.js
@@ -1,2 +1,8 @@
 // Atividade de Fixação 2 - Tipos Primitivos, Tipagem Dinâmica e Operações Aritméticas
 
+const base = 5;
+let height = 8;
+const area = base*  height;
+const perimeter = base + height + base + height;
+
+console.log(base, height, area, perimeter);
