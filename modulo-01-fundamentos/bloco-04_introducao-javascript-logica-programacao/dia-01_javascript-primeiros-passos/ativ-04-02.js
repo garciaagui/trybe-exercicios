29293f3259d14cfed3d6a428004ecb895d504404
Guilherme Garcia <garciaguig@gmail.com>
2022-05-12 20:06:25 -0300

    Atividade 4.2: Finalização + Alterações no README

--- modulo-01-fundamentos/bloco-04_introducao-javascript-logica-programacao/dia-01_javascript-primeiros-passos/ativ-04-02.js
+++ modulo-01-fundamentos/bloco-04_introducao-javascript-logica-programacao/dia-01_javascript-primeiros-passos/ativ-04-02.js
@@ -1,1 +1,13 @@
 // Atividade de Fixação 4.2 - Operadores OR (||)
+
+let weekDay = "sexta";
+
+if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira") {
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D")
+
+} else if (weekDay === "sábado" || weekDay === "domingo") {
+    console.log("FINALMENTE, descanso merecido UwU")
+
+} else {
+    console.log("Dia da semana inválido.")
+}
