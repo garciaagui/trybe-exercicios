82dd9d525442ad373abb96ce86e96fcdce495959
Guilherme Garcia <garciaguig@gmail.com>
2022-06-01 20:50:47 -0300

    Criação da pasta do dia e README.md + Cópia dos arquivos do Exercício do dia anterior

---
+++ modulo-01-fundamentos/bloco-06_html-css-forms-flexbox-responsivo/dia-02_bibliotecas-frameworks/script.js
@@ -0,0 +1,42 @@
+// Declaração das variáveis
+const userNameInput = document.querySelector('#user-name');
+const emailInput = document.querySelector('#user-email');
+const questionInput = document.querySelector('#question');
+const checkboxPermission = document.querySelector('#checkbox-permission');
+const imageInput = document.querySelector('#images');
+const submitBtn = document.querySelector('#submit-btn');
+
+// Funções
+function validateInputValues() {
+  username = userNameInput.value;
+  invalidUsername = username < 10 || username > 40;
+
+  email = emailInput.value;
+  invalidEmail = email < 10 || email > 50;
+
+  answer = questionInput.value;
+  invalidAnswer = answer > 500;
+
+  if (invalidUsername || invalidEmail || invalidAnswer) {
+    window.alert('Dados Inválidos');
+  } else {
+    window.alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
+  }
+}
+
+window.addEventListener('load', () => {
+  imageInput.disabled = true;
+})
+
+checkboxPermission.addEventListener('click', () => {
+  if(checkboxPermission.checked == false) {
+    imageInput.disabled = true;
+  } else {
+    imageInput.disabled = false;
+  }
+})
+
+submitBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  validateInputValues();
+})
