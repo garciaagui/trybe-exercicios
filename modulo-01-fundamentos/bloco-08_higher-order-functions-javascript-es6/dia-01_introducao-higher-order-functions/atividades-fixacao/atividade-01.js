b7acb1a08d413b1428503eb2b6e0ff35dfc25b91
Guilherme Garcia <garciaguig@gmail.com>
2022-06-22 14:50:31 -0300

    Conclusão da Atividade de Fixação

---
+++ modulo-01-fundamentos/bloco-08_higher-order-functions-javascript-es6/dia-01_introducao-higher-order-functions/atividades-fixacao/atividade-01.js
@@ -0,0 +1,9 @@
+const wakeUp = () => { return 'Acordando!!' };
+
+const takeBreakfast = () => { return 'Bora tomar café!!' };
+
+const goToBed = () => { return 'Partiu dormir!!' };
+
+const doingThings = (func) => { return func };
+
+console.log(doingThings(wakeUp()));
