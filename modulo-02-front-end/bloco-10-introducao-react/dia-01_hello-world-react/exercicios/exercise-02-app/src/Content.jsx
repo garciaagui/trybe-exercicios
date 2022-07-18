1b9970a7fd8c93100b59ae6571c530c40b6ce848
Guilherme Garcia <garciaguig@gmail.com>
2022-07-18 19:37:09 -0300

    Conclusão do Exercício 02 + Bônus

---
+++ modulo-02-front-end/bloco-10-introducao-react/dia-01_hello-world-react/exercicios/exercise-02-app/src/Content.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const conteudos = [
+  {
+    conteudo: 'High Order Functions',
+    bloco: 8,
+    status: 'Aprendido'
+  },
+  {
+    conteudo: 'Composicao de Componentes',
+    bloco: 11,
+    status: 'Aprendendo',
+  },
+  {
+    conteudo: 'Composicao de Estados',
+    bloco: 12,
+    status: 'Aprenderei'
+  },
+  {
+    conteudo: 'Redux',
+    bloco: 16,
+    status: 'Aprenderei'
+  },
+];
+
+const showContent = (obj, key) => {
+  const { conteudo, bloco, status } = obj;
+  return (
+    <section key={key} className="content">
+      <span className="subject">O conteúdo é: {conteudo}</span><br/>
+      <span>Status: {status}</span><br/>
+      <span>Bloco: {bloco}</span>
+    </section>
+  );
+}
+
+class Content extends React.Component {
+  render() {
+    return (
+      <section className="content-section">{conteudos.map((conteudo, index) => showContent(conteudo, index))}</section>
+    )
+  }
+}
+
+export default Content
