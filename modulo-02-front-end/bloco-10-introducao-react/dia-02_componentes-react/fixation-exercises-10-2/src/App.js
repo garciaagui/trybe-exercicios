99a218350a72abeafb14aeebcddf120fa3902fe8
Guilherme Garcia <garciaguig@gmail.com>
2022-07-19 21:28:55 -0300

    Conclusão da Atividade de Fixação 02

--- modulo-02-front-end/bloco-10-introducao-react/dia-02_componentes-react/fixation-exercises-10-2/src/App.js
+++ modulo-02-front-end/bloco-10-introducao-react/dia-02_componentes-react/fixation-exercises-10-2/src/App.js
@@ -1,14 +1,48 @@
-import Image from './Image';
+import React from 'react';
+import './App.css';
+import Order from './Components/Order';
 
+class App extends React.Component {
+  render() {
+    const headphone = {
+      id: 102,
+      user: "cena@gmail.com",
+      product: "Razer Headphone",
+      price: {
+        value: 99.99,
+        currency: "dollars"
+      }
+    };
 
-function App() {
-  const imgSrc = 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg';
-  const altText = 'Cute cat staring';
-  return (
-    <main>
-      <Image source={imgSrc} alternativeText={altText}/>
-    </main>
-  );
+    const energyDrink = {
+      id: 77,
+      user: "cena@gmail.com",
+      product: "Monster 500mL",
+      price: {
+        value: 9.99,
+        currency: "dollars"
+      }
+    };
+
+    return (
+      <div className="App">
+        <h1> Orders recently created </h1>
+         <Order order={headphone}/>
+         <Order order={energyDrink}/>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// Atividade Fixação 01
+// function App() {
+//   const imgSrc = 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg';
+//   const altText = 'Cute cat staring';
+//   return (
+//     <main>
+//       <Image source={imgSrc} alternativeText={altText}/>
+//     </main>
+//   );
+// }
