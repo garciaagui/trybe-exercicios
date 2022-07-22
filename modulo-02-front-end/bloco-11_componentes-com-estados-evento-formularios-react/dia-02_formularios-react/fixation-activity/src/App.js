a17ca38bbab18a28a0c5e13133e01fd69c0ebb21
Guilherme Garcia <garciaguig@gmail.com>
2022-07-22 17:15:20 -0300

    Conclusão do passo 02 da Atividade de Fixação + Alterações no README do dia

--- modulo-02-front-end/bloco-11_componentes-com-estados-evento-formularios-react/dia-02_formularios-react/fixation-activity/src/App.js
+++ modulo-02-front-end/bloco-11_componentes-com-estados-evento-formularios-react/dia-02_formularios-react/fixation-activity/src/App.js
@@ -7,12 +7,19 @@ class App extends Component {
 
     this.state = {
       userName: '',
+      userEmail: '',
+      userRegion: '',
+      userInfo: '',
+      userAge: '',
     }
   }
 
   handleChange = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value
+
     this.setState({
-      userName: target.value,
+      [name]: value
     })
   }
 
@@ -22,14 +29,18 @@ class App extends Component {
         <h1>Formulário - Atividade de Fixação</h1>
         <form>
           <div>
-            <label for="user-name">Nome do Usuário</label>
-            <input type="text" name="user-name" value={this.state.userName} onChange={this.handleChange}/>
+            <label htmlFor="userName">
+              Nome do Usuário
+             <input type="text" id="userName" name="userName" value={this.state.userName} onChange={this.handleChange}/>
+            </label>
           </div>
           <div>
-            <label for="user-email">E-mail do Usuário</label>
-            <input name="user-email" type="email"/>
+            <label htmlFor="userEmail">
+              E-mail do Usuário
+              <input type="email" id="userEmail" name="userEmail" value={this.state.userEmail} onChange={this.handleChange}/>
+            </label>
           </div>
-          <select name="user-region">
+          <select name="userRegion" id="userRegion" value={this.state.userRegion} onChange={this.handleChange}>
             <option value="">-- Selecione a região do Brasil onde você mora --</option>
             <option value="Norte">Norte</option>
             <option value="Nordeste">Nordeste</option>
@@ -38,11 +49,13 @@ class App extends Component {
             <option value="Sul">Sul</option>
           </select>
           <label>Fale um pouco sobre você :)
-            <textarea></textarea>
+            <textarea id="userInfo" name="userInfo" value={this.state.userInfo} onChange={this.handleChange}/>
           </label>
           <div>
-            <label for="user-age">Você é maior de 18 anos?</label>
-            <input name="user-age" type="checkbox"/>
+            <label htmlFor="userAge">
+              Você é maior de 18 anos?
+              <input name="userAge" id="userAge" type="checkbox" value={this.state.userInfo} onChange={this.handleChange}/>
+            </label>
           </div>
         </form>
       </main>
@@ -51,3 +64,53 @@ class App extends Component {
 }
 
 export default App;
+
+// PASSO 01
+// class App extends Component {
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       userName: '',
+//     }
+//   }
+
+//   handleChange = ({ target }) => {
+//     this.setState({
+//       userName: target.value,
+//     })
+//   }
+
+//   render() {
+//     return(
+//       <main>
+//         <h1>Formulário - Atividade de Fixação</h1>
+//         <form>
+//           <div>
+//             <label for="user-name">Nome do Usuário</label>
+//             <input type="text" name="user-name" value={this.state.userName} onChange={this.handleChange}/>
+//           </div>
+//           <div>
+//             <label for="user-email">E-mail do Usuário</label>
+//             <input name="user-email" type="email"/>
+//           </div>
+//           <select name="user-region">
+//             <option value="">-- Selecione a região do Brasil onde você mora --</option>
+//             <option value="Norte">Norte</option>
+//             <option value="Nordeste">Nordeste</option>
+//             <option value="Centro-Oeste">Centro-Oeste</option>
+//             <option value="Sudeste">Sudeste</option>
+//             <option value="Sul">Sul</option>
+//           </select>
+//           <label>Fale um pouco sobre você :)
+//             <textarea></textarea>
+//           </label>
+//           <div>
+//             <label for="user-age">Você é maior de 18 anos?</label>
+//             <input name="user-age" type="checkbox"/>
+//           </div>
+//         </form>
+//       </main>
+//     );
+//   }
+// }
