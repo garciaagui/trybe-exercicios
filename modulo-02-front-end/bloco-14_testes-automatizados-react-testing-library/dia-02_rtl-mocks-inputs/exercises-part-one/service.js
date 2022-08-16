fad325b69285af0a4c15b0e4dacee0f06e0f39f2
Guilherme Garcia <garciaguig@gmail.com>
2022-08-15 22:12:26 -0300

    Conclusão final da Parte I dos Exercícios do dia

--- modulo-02-front-end/bloco-14_testes-automatizados-react-testing-library/dia-02_rtl-mocks-inputs/exercises-part-one/service.js
+++ modulo-02-front-end/bloco-14_testes-automatizados-react-testing-library/dia-02_rtl-mocks-inputs/exercises-part-one/service.js
@@ -1,3 +1,5 @@
+const fetch = require('node-fetch');
+
 const generateRandomNumber = () => Math.floor(Math.random() * 100);
 
 const returnsUppercase = (str) => str.toUpperCase();
@@ -6,9 +8,17 @@ const returnsFirstLetter = (str) => str[0];
 
 const joinsTwoWords = (firstStr, secondStr) => firstStr+secondStr;
 
+const fetchDogImage = () => {
+    const url = 'https://dog.ceo/api/breeds/image/random';
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => data);
+}
+
 module.exports = { 
   generateRandomNumber,
   returnsUppercase,
   returnsFirstLetter,
   joinsTwoWords,
+  fetchDogImage,
 }
--- modulo-02-front-end/bloco-14_testes-automatizados-react-testing-library/dia-02_rtl-mocks-inputs/exercises-part-one/service.test.js
+++ modulo-02-front-end/bloco-14_testes-automatizados-react-testing-library/dia-02_rtl-mocks-inputs/exercises-part-one/service.test.js
@@ -42,34 +42,72 @@ describe('Implementações para a função generateRandomNumber', () => {
 })
 
 describe('Implementações para a função returnsUppercase', () => {
-  it('Testa o mock de uma nova funcionalidade que retorna a string passada como parâmetro em caixa baixa', () => {
-    service.returnsUppercase = jest.fn().mockImplementationOnce((str) => str.toLowerCase());
-    
-    expect(service.returnsUppercase('Batman')).toBe('batman');
-    expect(service.returnsUppercase).toHaveBeenCalled();
-    expect(service.returnsUppercase).toHaveBeenCalledTimes(1);
-    expect(service.returnsUppercase).toHaveBeenCalledWith('Batman');
+  it('Testa o mock de uma nova funcionalidade que retorna a string passada como parâmetro em caixa baixa', () => {    
+    const mockUpperCase = jest
+      .spyOn(service, 'returnsUppercase')
+        .mockImplementationOnce((str) => str.toLowerCase());
+
+    expect(mockUpperCase('bAtmAn')).toBe('batman');
+    expect(mockUpperCase).toHaveBeenCalled();
+    expect(mockUpperCase).toHaveBeenCalledTimes(1);
+    expect(mockUpperCase).toHaveBeenCalledWith('bAtmAn');
+
+    service.returnsUppercase.mockRestore();
+    expect(service.returnsUppercase('bAtmAn')).toBe('BATMAN');
   });
 })
 
 describe('Implementações para a função returnsFirstLetter', () => {
-  it('Testa o mock de uma nova funcionalidade que retorna a última letra da string passada como parâmetro', () => {
-    service.generateRandomNumber = jest.fn().mockImplementationOnce((a, b) => a / b);
+  it('Testa o mock de uma nova funcionalidade que retorna a string passada como parâmetro em caixa baixa', () => {
+    const mockFirstLetter = jest
+      .spyOn(service, 'returnsFirstLetter')
+        .mockImplementationOnce((str) => str[str.length - 1]);
     
-    expect(service.generateRandomNumber(4,2)).toBe(2);
-    expect(service.generateRandomNumber).toHaveBeenCalled();
-    expect(service.generateRandomNumber).toHaveBeenCalledTimes(1);
-    expect(service.generateRandomNumber).toHaveBeenCalledWith(4, 2);
+    expect(mockFirstLetter('Wolverine')).toBe('e');
+    expect(mockFirstLetter).toHaveBeenCalled();
+    expect(mockFirstLetter).toHaveBeenCalledTimes(1);
+    expect(mockFirstLetter).toHaveBeenCalledWith('Wolverine');
+
+    service.returnsFirstLetter.mockRestore();
+    expect(service.returnsFirstLetter('Wolverine')).toBe('W');
   });
 })
 
 describe('Implementações para a função joinsTwoWords', () => {
-  // it('Testa o mock de uma nova funcionalidade de divisão com dois parâmetros', () => {
-  //   service.generateRandomNumber = jest.fn().mockImplementationOnce((a, b) => a / b);
+  it('Testa o mock de uma nova funcionalidade que concatena três strings', () => {
+    const mockJoinsTwoWords = jest
+      .spyOn(service, 'joinsTwoWords')
+        .mockImplementationOnce((firstStr, secondStr, thirdStr) => firstStr+secondStr+thirdStr);
     
-  //   expect(service.generateRandomNumber(4,2)).toBe(2);
-  //   expect(service.generateRandomNumber).toHaveBeenCalled();
-  //   expect(service.generateRandomNumber).toHaveBeenCalledTimes(1);
-  //   expect(service.generateRandomNumber).toHaveBeenCalledWith(4, 2);
-  // });
+    expect(mockJoinsTwoWords('Game','Of','Thrones')).toBe('GameOfThrones');
+    expect(mockJoinsTwoWords).toHaveBeenCalled();
+    expect(mockJoinsTwoWords).toHaveBeenCalledTimes(1);
+    expect(mockJoinsTwoWords).toHaveBeenCalledWith('Game','Of','Thrones');
+
+    service.joinsTwoWords.mockRestore();
+    expect(service.joinsTwoWords('Game', 'Of')).toBe('GameOf');
+  });
+})
+
+describe ('Implementações para a função fetchDogImage', () => {
+  service.fetchDogImage = jest.fn();
+  afterEach(service.fetchDogImage.mockReset);
+
+  it('Testa o retorno quando a requisição da API é bem sucedida', async () => {
+    service.fetchDogImage.mockResolvedValue('request success');
+
+    service.fetchDogImage();
+    expect(service.fetchDogImage).toHaveBeenCalled();
+    expect(service.fetchDogImage).toHaveBeenCalledTimes(1);
+    await expect(service.fetchDogImage()).resolves.toBe("request success");
+    expect(service.fetchDogImage).toHaveBeenCalledTimes(2);
+  })
+
+  it('Testa o retorno quando a requisição da API falha', async () => {
+    service.fetchDogImage.mockRejectedValue("request failed");
+
+    expect(service.fetchDogImage).toHaveBeenCalledTimes(0);
+    await expect(service.fetchDogImage()).rejects.toMatch("request failed");
+    expect(service.fetchDogImage).toHaveBeenCalledTimes(1);
+  });
 })
