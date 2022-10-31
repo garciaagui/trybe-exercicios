e3074a3c1b448de488f7757ea9003693845c4fcb
Guilherme Garcia <garciaguig@gmail.com>
2022-10-31 09:54:47 -0300

    feat: Conclusão dos Exercícios 01 e 02 - continuação do 'Cacau Trybe'

--- modulo-03-back-end/bloco-22-secao-04_introducao-ao-desenvolvimento-web-com-nodejs/dia-03_nodejs-testes-de-integracao/cacau-trybe/tests/integration/chocolates.test.js
+++ modulo-03-back-end/bloco-22-secao-04_introducao-ao-desenvolvimento-web-com-nodejs/dia-03_nodejs-testes-de-integracao/cacau-trybe/tests/integration/chocolates.test.js
@@ -123,4 +123,17 @@ describe('Testando a API Cacau Trybe', function () {
       ]);
     });
   });
+
+  describe('Usando o método GET em /chocolates/total', function () {
+    it('Retorna a quantidade total de chocolates', async function () {
+      const response = await chai
+        .request(app)
+        .get('/chocolates/total');
+
+      expect(response.status).to.be.equal(200);
+      expect(response.body).to.deep.equal({
+        "totalChocolates": 4
+      });
+    });
+  });
 });
