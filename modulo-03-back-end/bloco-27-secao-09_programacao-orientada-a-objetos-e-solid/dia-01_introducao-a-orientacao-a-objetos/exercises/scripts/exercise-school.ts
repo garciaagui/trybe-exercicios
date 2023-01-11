fddf2ef80c2a6aab00ff5456d2821d572f6fe651
Guilherme Garcia <garciaguig@gmail.com>
2023-01-11 15:39:44 -0300

    :sparkles: feat: Adiciona o exercício 02

--- modulo-03-back-end/bloco-27-secao-09_programacao-orientada-a-objetos-e-solid/dia-01_introducao-a-orientacao-a-objetos/exercises/scripts/exercise-school.ts
+++ modulo-03-back-end/bloco-27-secao-09_programacao-orientada-a-objetos-e-solid/dia-01_introducao-a-orientacao-a-objetos/exercises/scripts/exercise-school.ts
@@ -4,11 +4,11 @@ class Student {
   private _examsGrades: number[];
   private _assignmentsGrades: number[];
 
-  constructor(r: string, n: string, en: number[], an: number[]) {
+  constructor(r: string, n: string) {
     this._enrollment = r;
     this._name = n;
-    this._examsGrades = en;
-    this._assignmentsGrades = an;
+    this._examsGrades = [];
+    this._assignmentsGrades = [];
   }
 
   get enrollment(): string {
@@ -58,7 +58,7 @@ class Student {
   }
 
   set assignmentsGrades(arr: number[]) {
-    if (arr.length !== 4) {
+    if (arr.length !== 2) {
       console.log('Error: 2 grades must be added.');
     }
     else {
@@ -66,6 +66,31 @@ class Student {
       console.log(`New assignments grades added: '${this._assignmentsGrades}'!`);
     }
   }
+
+  sumGrades(): number {
+    const arr = this._examsGrades.concat(this._assignmentsGrades);
+    let sum = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+
+    return sum;
+  }
+
+  avgGrades(): number {
+    const sum = this.sumGrades();
+    const arr = this._examsGrades.concat(this._assignmentsGrades)
+    const avg = sum / arr.length;
+
+    return avg;
+  }
 }
 
-const student1 = new Student('000000', 'Guilherme', [8, 6, 9, 10], [10, 8]);
+const student1 = new Student('000000', 'Guilherme');
+
+student1.examsGrades = [8, 6, 10, 9];
+student1.assignmentsGrades = [5, 10];
+
+console.log(`${student1.name}'s total score: ${student1.sumGrades()}`);
+console.log(`${student1.name}'s average score: ${student1.avgGrades()}`);
